f8a3c622c3afbe7ebaf11e2adaf5169a9033af8d
juguanghui <java_program@aliyun.com>
2017-03-29 14:06:08 +0800

    Ldap test

--- src/test/java/ldap/db2ldap/LdapTest.java
+++ src/test/java/ldap/db2ldap/LdapTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ldap.core.LdapTemplate;
 
 public class LdapTest {
